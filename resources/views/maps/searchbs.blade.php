e9181bddac0625f254dfc0a3fefe874ccbf47e94
tmali16 <don.kg16@gmail.com>
2024-01-25 17:23:39 +0600

    show address

--- resources/views/maps/searchbs.blade.php
+++ resources/views/maps/searchbs.blade.php
@@ -26,8 +26,9 @@
                                 <tbody>
                                 @foreach($items as $item)
                                     <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                                        <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                            {{$item->sector_name ?? $item->lac . "_" . $item->cid}}
+                                        <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white flex flex-col">
+                                            <div>{{$item->sector_name ?? $item->lac . "_" . $item->cid}}</div>
+                                            <div class="text-xs text-gray-700">{{$item->address}}</div>
                                         </td>
                                         <td class="px-6 py-4">
                                             <a class="font-medium text-blue-600 dark:text-blue-500 hover:underline" target="_blank" href="{{"https://info.o.kg/yourLocation.html?lat=".$item->site_lat."&lon=".$item->site_lon."&az=".$item->azimuth."&cr=1-500&abw=64"}}">Открыть</a>
